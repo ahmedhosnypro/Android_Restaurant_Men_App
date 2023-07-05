9a5372a2463e147dda9031014c3c02f1bd566ba4
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-05 17:41:56 +0300

    stage 1/5: The title

---
+++ Restaurant Menu App/task/src/main/java/org/hyperskill/ordersmenu/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.hyperskill.ordersmenu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import org.hyperskill.ordersmenu.theme.PlayOrdersMenuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlayOrdersMenuTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("Orders Menu")
+                }
+            }
+        }
+    }
+}
